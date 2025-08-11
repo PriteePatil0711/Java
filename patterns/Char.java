24778e6ca710dcce65151a73abed00d5019e2cf8
priteepatil <priteepatil252525@gmail.com>
2025-08-11 09:49:02 +0530

    Add patterns folder as normal folder

---
+++ patterns/Char.java
@@ -0,0 +1,16 @@
+public class Char
+{
+    public static void main(String args[])
+    {
+        int n=4;
+        char ch='A';
+        for(int line=1; line<=n; line++)
+        {
+            for(int chars=1; chars<=line; chars++)
+            {
+                System.out.print(ch);
+                ch++;
+            }
+        }
+    }
+}
